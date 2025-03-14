41eb39c74e1f4715b80750a7d598ecb6bccd7dbb
Manjay Kumar <103574933+manjay6428@users.noreply.github.com>
2025-03-14 21:49:46 +0530

    added redux-persist and jobs integration

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,6 @@
 export const USER_BASE_URL = "http://localhost:8000/api/user";
 export const JOB_BASE_URL = "http://localhost:8000/api/job";
+export const APPLICATION_BASE_URL = "http://localhost:8000/api/application";
 
 export const getInitialName = (userName) => {
   if (!userName) return "CN";
