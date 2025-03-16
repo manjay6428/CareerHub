d1177297ed459a399d11bc93a6ca0df3812e1dd2
Manjay Kumar <103574933+manjay6428@users.noreply.github.com>
2025-03-16 19:30:34 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -100,8 +100,8 @@ const loginUser = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
-        httpsOnly: true,
-        sameSite: "strict",
+        httpOnly: true,
+        sameSite: "none",
       })
       .json({
         message: `Welcome back ${user.fullName}`,
